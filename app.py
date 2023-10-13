c9b307129096faaed64cc9d63ce69e63b12b7f24
daridev <darideveloper@gmail.com>
2023-10-13 14:04:07 -0600

    feat (app): webwook and basic response

---
+++ app.py
@@ -0,0 +1,84 @@
+import os
+from flask import Flask, request
+from dotenv import load_dotenv
+from sender import send_message
+
+load_dotenv ()
+
+app = Flask(__name__)
+
+WP_WEBOOK_TOKEN = os.getenv ('WP_WEBOOK_TOKEN')
+
+@app.get ('/')
+def index ():
+    """ Home page """
+    
+    return {
+        "status": "success",
+        "message": "service running",
+        "data": []
+    }
+    
+@app.get ('/webhook/')
+def webhook_subscribe ():
+    """ Webhook for whatsapp """
+    
+    # Get url&get variables
+    hub_mode = request.args.get ('hub.mode', '')
+    hub_challenge = request.args.get ('hub.challenge', '')
+    hub_verify_token = request.args.get ('hub.verify_token', '')
+    
+    if hub_mode == "subscribe": 
+    
+        # Validate token
+        if hub_verify_token != WP_WEBOOK_TOKEN:
+            return ({
+                "status": "error",
+                "message": "invalid token",
+                "data": []
+            }, 401)
+        
+        return hub_challenge
+    
+    return ({
+        "status": "error",
+        "message": "invalid request",
+        "data": []
+    }, 400)
+
+@app.post ('/webhook/')
+def message ():
+    
+    # Get all post data
+    data = request.get_json ()
+    
+    # Get message data
+    try:
+        message_data = data["entry"][0]["changes"][0]["value"]["messages"][0]
+        message_phone = message_data["from"]
+        message_text = message_data["text"]["body"]
+    except Exception as e:
+        return ({
+            "status": "error",
+            "message": "invalid request",
+            "data": []
+        }, 200)
+        
+    # Fix phone number format
+    if message_phone.startswith ("521"):
+        message_phone = message_phone.replace ("521", "52")
+    
+    # Send message
+    try:
+        send_message(message_phone, message_text)
+    except Exception as e:
+        pass
+    
+    return {
+        "status": "success",
+        "message": "message sent",
+        "data": []
+    }
+    
+if __name__ == "__main__":
+    app.run (port=5000)
--- sender.py
+++ sender.py
@@ -7,28 +7,46 @@ load_dotenv ()
 
 WP_TOKEN = os.getenv ('WP_TOKEN')
 
-def send_message (phone:int, message:str):
+def send_message (phone:int, message:str="", template:str=""):
     """ Send a message in whatsapp using the meta api
 
     Args:
         phone (int): to whom the message will be sent
-        message (str): message to be sent
+        message (str): custom message to be sent
+        template (str): template to be sent
     """
     
+    # Validate message or template
+    if not message and not template:
+        raise Exception ("You must provide a message or a template")
 
     url = "https://graph.facebook.com/v17.0/147654091761491/messages"
 
-    payload = json.dumps({
-        "messaging_product": "whatsapp",
-        "to": f"{phone}",
-        "type": "template",
-        "template": {
-            "name": "hello_world",
-            "language": {
-            "code": "en_US"
+    payload = ""
+    if template:
+        payload = json.dumps({
+            "messaging_product": "whatsapp",
+            "to": f"{phone}",
+            "type": "template",
+            "template": {
+                "name": template,
+                "language": {
+                "code": "en_US"
+                }
             }
-        }
-    })
+        })
+        
+    if message:
+        payload = json.dumps({
+            "messaging_product": "whatsapp",
+            "to": f"{phone}",
+            "type": "text",
+            "text": { 
+                "preview_url": True,
+                "body": message
+            }
+        })
+        
     headers = {
         'Authorization': f'Bearer {WP_TOKEN}',
         'Content-Type': 'application/json'
@@ -38,5 +56,3 @@ def send_message (phone:int, message:str):
     res.raise_for_status ()
     
     print (f"message send to {phone}")
-
-send_message (527295162472, "hello world")
